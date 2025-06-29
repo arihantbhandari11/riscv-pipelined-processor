74554cf28ad66d234278cb40f9e0ecac22411b33
arihantbhandari11 <ee230002012@iiti.ac.in>
2025-06-29 21:45:18 +0530

    Create pipeline_processor.v

---
+++ pipeline_processor.v
@@ -0,0 +1,12 @@
+module pipeline_processor(input clk, reset,  
+                          output  [31:0] WriteDataM, DataAdrM,  
+                          output MemWriteM); 
+ 
+  wire [31:0] PCF, InstrF, ReadDataM; 
+   
+  // instantiate processor and memories 
+  riscv riscv(clk, reset, PCF, InstrF, MemWriteM, DataAdrM,  
+              WriteDataM, ReadDataM); 
+  imem imem(PCF, InstrF); 
+  dmem dmem(clk, MemWriteM, DataAdrM, WriteDataM, ReadDataM); 
+endmodule 
